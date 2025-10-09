553fbf6ee49c8abf6a2ed9b61432b76383393eb2
wzx11111 <3279285308@qq.com>
2025-10-09 17:27:03 +0800

    first commit

---
+++ src/components/mobanyufa.vue
@@ -0,0 +1,32 @@
+<!-- mobanyufa.vue -->
+<template>
+  <div class="mobanyufa">
+    <h3>模板语法</h3>
+    <p>{{ msg }}</p>
+    <p>{{ number +100 }}</p>
+    <div>{{ !ok ? "ok1" : "ok2" }}</div>
+    
+    <div>{{ rawHtml }}</div>
+    <div v-html="rawHtml"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mobanyufa",
+  data() {
+    return {
+      msg: "adadadadad",
+      number : 1,
+      ok: true,
+      rawHtml: "<a href='https://www.baidu.com'>百度</a>"
+    }
+  },
+}
+</script>
+
+<style scoped>
+.mobanyufa {
+    color: yellow;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home-container">
+        <h1>VUE3教学</h1>
+        <hr>
+        <Mobanyufa></Mobanyufa>
+    </div>
+</template>
+
+<script>
+import Mobanyufa from '../components/mobanyufa.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        Mobanyufa
+    },
+
+}
+</script>
+
+<style scoped>
+.home-container{
+    position: relative;
+    z-index: 10;
+}
+h1{
+    z-index: 11;
+    color: aqua;
+}
+</style>
