3ebc9fc7a0b895030f76a260dd8a77ecfc8a3b6b
wzx11111 <3279285308@qq.com>
2025-10-20 11:33:17 +0800

    add 4

---
+++ src/components/shijianchuancan.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="shijianchuancan">
+        <h3>事件传参</h3>
+        <button @click="count1">add 454</button>
+        <p>count is {{ countNum }}</p>
+
+        <button @click="newCount(countNum)">newCount 222</button>
+        <hr>
+    </div>
+
+</template>
+
+
+<script>
+
+export default { 
+    name: 'shijianchuancan',
+    data() {
+        return {
+            countNum: 0,
+            newCount: 20
+        }
+    },
+    methods: {
+        count1(e) {
+            console.log(e, '454545454564565654');
+            console.log(e.target,'e.targetet.target.target');
+            console.log(e.currentTarget,'e.currentTarget.currentTarget.currentTarget.');
+            console.log(e.target.innerHtml,'e.target.innerHtml.innerHtml.innerHtml');
+            e.target.innerHtml = "添加添加" + this.countNum;
+            this.countNum += 454;
+        },
+        newCount(msg) {
+            console.log(msg, 'msgmsgmsgmsg');
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.shijianchuancan{
+    color: royalblue;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -5,6 +5,10 @@
         <Mobanyufa></Mobanyufa>
         <Shuxingbangding></Shuxingbangding>
         <Tiaojianxuanran></Tiaojianxuanran>
+        <Liebiaoxuanran></Liebiaoxuanran>
+        <Keyguanlizhuangtai></Keyguanlizhuangtai>
+        <Shijianchuli></Shijianchuli>
+        <Shijianchuancan></Shijianchuancan>
     </div>
 </template>
 
@@ -12,13 +16,21 @@
 import Mobanyufa from '../components/mobanyufa.vue';
 import Shuxingbangding from '../components/shuxingbangding.vue';
 import Tiaojianxuanran from '../components/tiaojianxuanran.vue';
+import Liebiaoxuanran from '../components/liebiaoxuanran.vue';
+import Keyguanlizhuangtai from '../components/keyguanlizhuangtai.vue';
+import Shijianchuli from '../components/shijianchuli.vue';
+import Shijianchuancan from '../components/shijianchuancan.vue';
 
 export default {
     name: 'Home',
     components: {
         Mobanyufa,
         Shuxingbangding,
-        Tiaojianxuanran
+        Tiaojianxuanran,
+        Liebiaoxuanran,
+        Keyguanlizhuangtai,
+        Shijianchuli,
+        Shijianchuancan
     },
 
 }
